ac71d74e81e2253908a15a146bfd7b3d6aa69d46
Yua Surya <yuaprofessional@gmail.com>
2026-04-15 15:33:11 +0530

    [Surya]Modify : Daily Wages

--- emp.py
+++ emp.py
@@ -1,13 +1,23 @@
                                                                                   
 import random
 
-def atd():
-        print("Welcome")
-        attendance = random.choice([0,1])
-        if (attendance == 1):
-               print("Present")
-        else:
-                print("Absent")
+def check_attendance():
+    print ("Welcome to Employee Wages Computation Program on Master Branch")
+    attendance = random.choice([1, 0])
+    return attendance
+
+
+def emp_daily_wage(): 
+    wage_per_hr = 20 
+    emp_check = check_attendance()
+
+    if emp_check == 1:
+        daily_wage = wage_per_hr * 8
+        print (f"The Employee is present for full day so the daily Wage is: {daily_wage}")
+    else:  
+        daily_wage = 0
+        print(f"Employee is absent for the day is:{daily_wage}")
+
 
 if __name__ == "__main__":
-        atd()
+    emp_daily_wage()
