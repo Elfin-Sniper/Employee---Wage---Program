01a57474832135c69962246b69aab9fdfe707f7a
Yua Surya <yuaprofessional@gmail.com>
2026-04-15 15:12:05 +0530

    [Surya]Modify : Attendance

--- emp.py
+++ emp.py
@@ -1,1 +1,13 @@
-print("Hello")
+                                                                                  
+import random
+
+def atd():
+        print("Welcome")
+        attendance = random.choice([0,1])
+        if (attendance == 1):
+               print("Present")
+        else:
+                print("Absent")
+
+if __name__ == "__main__":
+        atd()
